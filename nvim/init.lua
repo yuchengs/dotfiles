05f837f4928514e08d409173a0aa8a35ed02fef7
yuchengs <38407281+yuchengs@users.noreply.github.com>
2021-09-13 00:07:05 -0700

    Remove deprecated options

--- nvim/init.lua
+++ nvim/init.lua
@@ -164,13 +164,17 @@ require('gitsigns').setup {
   watch_index = {
     interval = 1000
   },
+  current_line_blame_opts = {
+    delay = 500,
+    virt_text_pos = 'eol'
+  },
   current_line_blame = true,
-  current_line_blame_delay = 500,
-  current_line_blame_position = 'eol',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil,    -- Use default
-  use_internal_diff = true,  -- If luajit is present
+  diff_opts = {
+    internal = true
+  }
 }
 -- Plugin name: indent_blankline
 -- :help indent_blankline
